cc12fd3e0eb2ef42f55f5ba920ba0a10a7ed06f0
wilson-huynh-webjet <wilson.huynh@webjet.com.au>
2024-09-16 09:37:32 +1000

    update header

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ function App() {
 
   return (
     <>      
-      <h1>Web 1 new</h1>
+      <h1 className="main-header">Shared Main Header</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
