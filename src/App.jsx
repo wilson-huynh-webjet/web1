706ad0402cd2c3c6788b4bbf21ca7713571696e6
wilson-huynh-webjet <wilson.huynh@webjet.com.au>
2024-09-16 18:07:16 +1000

    Add shared banner

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,21 @@
 import { useState } from 'react'
 import './App.css'
+import Banner from '@webjet/react/components/banner'
 
-function App() {
+function App () {
   const [count, setCount] = useState(0)
 
   return (
-    <>      
-      <h1 className="main-header">Shared Main Header</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
+    <>
+      <h1 className='main-header'>Shared App</h1>
+      <Banner>
+        <h3>Promo Banner </h3>
+      </Banner>
+      <div className='card'>
+        <button onClick={() => setCount(count => count + 1)}>
           count is {count}
-        </button>        
-      </div>      
+        </button>
+      </div>
     </>
   )
 }
